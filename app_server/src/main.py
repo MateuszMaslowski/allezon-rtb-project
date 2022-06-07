277dc6d589f8d1e8560d973719461f08a03ba37a
MateuszMaslowski <maslowskim255@gmail.com>
2022-06-07 22:03:30 +0200

    always return 204

--- app_server/src/main.py
+++ app_server/src/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/user_tags/view")
+async def add_view_tag(body, response: Response):
+    response.status_code = 204
+    print("View", body)
+    return
+
+
+@app.post("/user_tags/buy")
+async def add_buy_tag(body, response: Response):
+    response.status_code = 204
+    print("Buy", body)
+    return
